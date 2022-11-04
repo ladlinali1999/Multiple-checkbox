7e5d9a2c619e87c2e13ddc38d0de33c3e167366e
linali anil lad <linalilad3108@gmail.com>
2022-11-04 16:45:31 +0530

    checkbox commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Mcheckbox';
+  _courselist:course[] | undefined;
+  
+
+  ngOnInit(){
+    this.getcourses();
+  }
+
+  getcourses(){
+    this._courselist=[
+      {id:1,name:"C#",isselected:false},
+      {id:2,name:"Angular",isselected:false},
+      {id:3,name:"Asp.net",isselected:false},
+      {id:4,name:"SQL",isselected:false},
+      {id:5,name:"Python",isselected:false},
+
+
+    ]
+  }
+  onchange(){
+    console.log(this._courselist);
+  }
+
+}
+class course{
+  id:number | undefined;
+  name: string | undefined;
+  isselected: boolean | undefined;
 }
